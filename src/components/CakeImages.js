d31b965465f8725f6929c5ad856f23acebf3fba1
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-14 16:26:38 +0100

    adding to github

---
+++ src/components/CakeImages.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CakeImages = () => {
+    return (
+        <section>
+            <img src="https://www.rainbownourishments.com/wp-content/uploads/2022/02/vegan-strawberry-cake-1-1.jpg" alt="strawberry cake" width={200} height={200} />
+
+            <img src="https://biancazapatka.com/wp-content/uploads/2019/06/best-vegan-chocolate-cake-recipe-cherry-frosting-ganache.jpg" alt="chocolate cake" width={200} height={200} />
+
+            <img src="https://biancazapatka.com/wp-content/uploads/2018/07/vegan-crumble-cake-cheese-cake-blueberry.jpg" alt="blueberry cake" width={200} height={200} />
+
+        </section>
+    );
+};
+
+export default CakeImages;
